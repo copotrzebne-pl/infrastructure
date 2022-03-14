1c584de8aa1d43954a2d9f5395f89e68b667e3d4
Wojciech Krawczyk <wojciech.krawczyk@schibsted.com>
2022-03-14 23:02:06 +0100

    feat: Expose the most important information from the environment

---
+++ environments/prod/outputs.tf
@@ -0,0 +1,39 @@
+output "remote_state_bucket" {
+  value       = module.remote-state-s3-backend.state_bucket
+  description = "The S3 bucket to store the remote state file."
+}
+
+output "remote_state_replica_bucket" {
+  value       = module.remote-state-s3-backend.replica_bucket
+  description = "The S3 bucket to replicate the state S3 bucket."
+}
+
+output "s3_deployer_user_name" {
+  value       = module.cdn_with_s3_bucket.deployer_user_name
+  description = "IAM user name"
+}
+
+output "s3_deployer_access_key_id" {
+  value       = module.cdn_with_s3_bucket.deployer_access_key_id
+  description = "Access key ID"
+}
+
+output "s3_deployer_access_key_secret" {
+  value       = module.cdn_with_s3_bucket.deployer_access_key_secret
+  description = "Secret access key"
+}
+
+output "zone_id" {
+  value       = module.hosting_zone.zone_id
+  description = "The Hosted Zone ID"
+}
+
+output "zone_name" {
+  description = "Name of the hosted zone."
+  value       = module.hosting_zone.name
+}
+
+output "zone_name_servers" {
+  value       = module.hosting_zone.name_servers
+  description = "A list of name servers in associated (or default) delegation set"
+}
