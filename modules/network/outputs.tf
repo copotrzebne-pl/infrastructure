e9be2939e5f606f327fd11f93b28fe17c2a47e2b
Tomasz Ciesek <tomasz.ciesek@schibsted.pl>
2022-03-15 22:31:45 +0100

    style: apply tf fmt

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -5,15 +5,15 @@ output "azs" {
 
 output "vpc_id" {
   description = "Id of the VPC associated to the network."
-  value = module.vpc.vpc_id
+  value       = module.vpc.vpc_id
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets."
-    value = module.vpc.public_subnets
+  value       = module.vpc.public_subnets
 }
 
 output "private_subnets" {
   description = "List of IDs of private subnets."
-  value = module.vpc.private_subnets
+  value       = module.vpc.private_subnets
 }
