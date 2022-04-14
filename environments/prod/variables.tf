4987fbf79d8191dc8ba4465bc59c17d0b4007f54
Wojciech Krawczyk <wojciech.krawczyk@schibsted.com>
2022-04-14 23:39:57 +0200

    feat: Add english and ukrainian domain

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -8,6 +8,16 @@ variable "base_domain" {
   type        = string
 }
 
+variable "base_domain_en" {
+  description = "Base domain name for the project - english version"
+  type        = string
+}
+
+variable "base_domain_ua" {
+  description = "Base domain name for the project - ukraine version"
+  type        = string
+}
+
 variable "api_domain_name" {
   type        = string
   description = "A domain name for API"
