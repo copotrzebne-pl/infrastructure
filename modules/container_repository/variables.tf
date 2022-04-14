e2f125260c2f58b776016f8dc76ccde5f2c0645b
Wojciech Krawczyk <wojciech.krawczyk@schibsted.com>
2022-04-14 23:37:44 +0200

    feat: Remove obsolete elements

--- modules/container_repository/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "name" {
-  description = "Name of the repository."
-  type        = string
-}
--- modules/rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "host" {
-  description = "DB Instance host"
-  value       = aws_db_instance.default.domain
-}
-
-output "port" {
-  description = "DB Instance port"
-  value       = aws_db_instance.default.port
-}
